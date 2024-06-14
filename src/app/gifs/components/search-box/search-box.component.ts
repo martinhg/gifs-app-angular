454ab46f09217cd1a9473ce6913a77f9b3d8e534
Martin Herrera <martinherrera.g96@gmail.com>
2024-06-13 23:08:02 -0300

    Adding a viewchild into the search-box input

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
   template: `
     <h5>Buscar:</h5>
-    <input type="text" class="form-control" placeholder="Buscar gif...">
+    <input type="text" class="form-control" placeholder="Buscar gif..." (keyup.enter)="searchTag()" #searchInput>
   `,
 })
+/*
+  !Important:
+  * keyup = Cuando la tecla se levanto
+  * Keydown = Cuando el usuario empieza a hundir la tecla
+  * Keypress = Cuando ya presiono la tecla
+*/
 export class SearchBoxComponent {
+  @ViewChild('searchInput')
+  private tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor() {}
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({ newTag});
+  }
 
 }
