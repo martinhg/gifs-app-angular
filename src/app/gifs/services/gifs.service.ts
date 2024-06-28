ace04d9d91e164b5976ee54c7e39df0befe81092
Martin Herrera <martinherrera.g96@gmail.com>
2024-06-28 13:45:45 -0300

    Service validations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,8 +14,20 @@ export class GifsService {
   }
 
   searchTag(tag: string): void {
-    console.log({ tag });
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);
+  }
+
+  private organizeHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
+
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter(oldTag => oldTag !== tag);
+    }
 
     this._tagsHistory.unshift(tag);
+
+    this._tagsHistory = this.tagsHistory.splice(0, 10);
   }
 }
